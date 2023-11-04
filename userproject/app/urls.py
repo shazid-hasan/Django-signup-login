3ea120ef25be1ef8e92ee4ec1938994ed2432fcf
Shazid Hasan <amj.shazidhasan@gmail.com>
2023-11-05 00:33:09 +0600

    first commit

---
+++ userproject/app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path,include
+from . import views
+urlpatterns = [
+    path('',views.index),
+    path('registration',views.registration),
+    
+]
---
+++ userproject/app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render,HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def registration(request):
+    if request.method == 'POST':
+        user_form= UserCreationForm(request.POST)
+        if user_form.is_valid():
+            user_form.save()
+            return HttpResponse("success")
+    else:
+        user_form = UserCreationForm()
+    return render(request,'registration.html',{'user_form':user_form})     
+
+
+
